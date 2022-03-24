55a219ed51d24dc26cf6e603c305daa8d68e6084
jonaseal <72393212+jonaseal@users.noreply.github.com>
2022-03-24 14:58:24 -0300

    Update edujob.py

--- edujob.py
+++ edujob.py
@@ -5,3 +5,5 @@ url = 'https://www.w3schools.com/python/demopage.php'
 arquivo = open('cadastrarvaga.json',encoding='utf-8')
 objeto = json.load(arquivo)
 #resultado = requests.post(url, data = objeto)
+#print(resultado.status_code)
+print(objeto['cadastrarvaga'][0]['dadosdaempresa']['cnpj'])
